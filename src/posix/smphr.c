3cec01906b01e73c6e4043d86021c4570d34beb7
Marcin Smoczynski <smoczynski.marcin@gmail.com>
2015-07-10 21:14:39 +0200

    bad semaphore alloc bugfix

--- src/posix/smphr.c
+++ src/posix/smphr.c
@@ -5,12 +5,14 @@
 #include <semaphore.h>
 #include <time.h>
 
+#include <stdlib.h>
 #include <stdbool.h>
 
 bool smphr_init(smphr_t *s, unsigned int value)
 {
     s->value = value;
-    return sem_init((sem_t *)&s->handle, 0, value) == 0;
+    s->handle = (sem_t *)malloc(sizeof(sem_t));
+    return sem_init((sem_t *)s->handle, 0, value) == 0;
 }
 
 bool smphr_take(smphr_t *s, system_tick_t ticks)
@@ -18,15 +20,15 @@ bool smphr_take(smphr_t *s, system_tick_t ticks)
     struct timespec wait;
 
     if(ticks == 0) {
-        return sem_trywait((sem_t *)&s->handle) == 0;
+        return sem_trywait((sem_t *)s->handle) == 0;
     }
 
     system_ticks_to_timespec(ticks, &wait);
-    return sem_timedwait((sem_t *)&s->handle, &wait) == 0;
+    return sem_timedwait((sem_t *)s->handle, &wait) == 0;
 }
 
 bool smphr_give(smphr_t *s)
 {
-    return sem_post((sem_t *)&s->handle) == 0;
+    return sem_post((sem_t *)s->handle) == 0;
 }
 
