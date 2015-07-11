edc20dd682846bf23251eb570c1dd65620486e73
Marcin Smoczynski <smoczynski.marcin@gmail.com>
2015-07-11 18:54:38 +0200

    queue creation

--- src/posix/queue.c
+++ src/posix/queue.c
@@ -1,2 +1,23 @@
 #include <system/system.h>
 #include <system/queue.h>
+#include <system/smphr.h>
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stddef.h>
+
+bool queue_create(queue_t *q, size_t size, size_t elsize)
+{
+    q->size = size;
+    q->element_size = elsize;
+    q->elements = 0;
+    q->data = malloc(size * elsize);
+
+    return q->data != NULL && smphr_init(&q->sem, size);
+}
+
+size_t queue_elements_count(queue_t *q)
+{
+    return q->elements;
+}
--- src/queue.h
+++ src/queue.h
@@ -1,6 +1,21 @@
 #ifndef __SYSTEM_QUEUE_H__
 #define __SYSTEM_QUEUE_H__
 
+#include <system/system.h>
+#include <system/smphr.h>
 
+#include <stddef.h>
+#include <stdbool.h>
+
+typedef struct {
+    smphr_t sem;
+    size_t size;
+    size_t element_size;
+    size_t elements;
+    void *data;
+} queue_t;
+
+bool queue_create(queue_t *q, size_t size, size_t elsize);
+size_t queue_elements_count(queue_t *q);
 
 #endif /* __SYSTEM_QUEUE_H__ */
